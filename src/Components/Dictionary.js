d16519452494873ef526135629d46c496a79ff15
ananyeli24 <ananyelibatista@AdminisatorsMBP.lan>
2022-08-09 15:24:50 -0400

    Added empty Routes and Components AB

---
+++ src/Components/Dictionary.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { NavBar } from "./NavBar";
+
+
+export const Dictionary = () => {
+  return (
+    <div>Dictionary
+        <NavBar />
+    </div>
+  )
+}
---
+++ src/Components/RouterPage.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import {BrowserRouter, Routes,Route} from 'react-router-dom';
+import { Home } from './Home';
+import { Contact } from './Contact';
+import { Dictionary } from './Dictionary';
+import { News } from './News';
+
+export default function RouterPage() {
+  return (
+    <div>
+        <BrowserRouter>
+        <Routes>
+            
+              <Route path="/"  element={<Home/>} />
+              <Route path="contact" element={<Contact/>} />
+              <Route path="dictionary" element={<Dictionary/>} />
+              <Route path="news" element={<News/>} />
+
+        </Routes>
+        </BrowserRouter>
+    </div>
+  )
+}
+
+
