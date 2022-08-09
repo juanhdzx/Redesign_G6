0b124073d4bc5c294852a62c3f37ccde0d99a275
juanhdzx <102987890+juanhdzx@users.noreply.github.com>
2022-08-09 16:03:10 -0400

    updates 4pm

--- src/App.js
+++ src/App.js
@@ -1,40 +1,16 @@
 //import logo from './logo.svg';
-import React from 'react';
-import './App.css';
+ import React from 'react';
+ import './App.css';
 
-class App extends React.Component () {
-  constructor(props) {
-    super(props);
-    this.state = {
-      wikiSearchTerms: ''
-    }
+
+
+class App extends React.Component {
+  render (){
+    return (
+      <h1>Hello World</h1>
+    )
   }
+  
+};
 
-  useWikiSearchEngine = (e) => {
-
-  }
-
-  changeWkikiSearchTerms = (e) => {
-    this.setState({
-      wikiSearchTerms: e.target.value
-    })
-  }
-
-  render() {
-
-    let wikiSearchResults = []
-
-      return (
-        <div className="App">
-          <h1>Wikipedia</h1>
-          <form action="">
-            <input type="text" value='' onChange={this.changeWkikiSearchTerms} placeholder='Search Wikipedia Articles' />
-            <button type="submit" onClick={this.useWikiSearchEngine} ></button>
-    
-          </form>
-          {wikiSearchResults}
-        </div>
-      );
-    }
-  }
 export default App;
