44145ff45f9e0808772edde573dcaa5c7d67e4f0
EthanDoan3655 <37298042+EthanDoan3655@users.noreply.github.com>
2022-08-10 12:57:11 -0400

    Update Contact.js

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,9 +1,38 @@
 import React from 'react'
-import { NavBar } from './NavBar'
-export const Contact = () => {
+import NavBar from './NavBar'
+import styled from 'styled-components'
+import Validation from './Validation'
+import Footer from '../components/Footer'
+
+const Container = styled.div`
+    width: 100vw;
+    height: 100vh;
+     center;
+    background-color:#B6D0E2;
+     display:flex;
+     align-items:center;
+     justify-content:center;
+`;
+const Wrapper = styled.div`
+    
+    padding:20px;
+    background-color:white;
+    
+
+`;
+
+
+const Contact = () => {
   return (
-    <div>Contact
-        
-    </div>
-  )
+      <>
+    <NavBar/>
+    <Container>
+        <Wrapper>
+<Validation/>
+        </Wrapper>
+   
+    </Container>
+ </> )
 }
+
+export default Contact
